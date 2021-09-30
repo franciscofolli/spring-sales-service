3dff5ed3334a7c3b33c1b208414ace650ec67da9
Francisco Folli <franciscoabel2001@gmail.com>
2021-09-30 16:55:11 -0300

    uploading project draft

---
+++ src/main/java/com/folli/sales/SpringSalesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.folli.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSalesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSalesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folli/sales/SpringSalesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folli.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSalesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
